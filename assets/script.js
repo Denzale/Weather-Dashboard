8c032bba027aab21277bdaae3c90f261feb73e61
Denzale Houston <denzalethouston@gmail.com>
2020-09-22 18:36:03 -0700

    added Ajax

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,32 @@
-$(document).ready
+$(document).ready.function()
+
+let apiKey = "d7daca23bb60b5cdedb491704005803bS";
+  const state = $("input").val();
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=$'+inputValue.value+'&appid=$&units=metric`;
+  
+
+
+  $.ajax({
+    url: "URL GOES HERE",
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+});
+
+
+$.ajax({
+    url: "URL GOES HERE",
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+});
+
+
+
+$.ajax({
+    url: "URL GOES HERE",
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+});
