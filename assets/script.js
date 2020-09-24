363c7f01a10a8e95ccf04fbd90a02ff99780aea1
Denzale Houston <denzalethouston@gmail.com>
2020-09-23 23:20:02 -0700

    Working on ajax calls

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,38 @@
-$(document).ready.function()
+$(document).ready( function(){
 
 let apiKey = "d7daca23bb60b5cdedb491704005803bS";
-  const state = $("input").val();
+const state = $("input").val();
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=$'+inputValue.value+'&appid=$&units=metric`;
-  
-
-
-  $.ajax({
-    url: "URL GOES HERE",
+///first call
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=malibu&appid=d7daca23bb60b5cdedb491704005803bS",
     method: "GET"
 }).then(function (response) {
     console.log(response)
 });
 
+///second call
+function currentWeather() {
+    
 
 $.ajax({
-    url: "URL GOES HERE",
+    url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d7daca23bb60b5cdedb491704005803bS",
     method: "GET"
 }).then(function (response) {
     console.log(response)
 });
 
+}
 
 
+///third call
+function fiveDayCast() {
+    
 $.ajax({
-    url: "URL GOES HERE",
+    url: "pro.openweathermap.org/data/2.5/forecast/hourly?id={city ID}&appid={API key}",
     method: "GET"
 }).then(function (response) {
     console.log(response)
 });
+
+}})
