cc92d3dd6c2247494216416ce12e49faab3bfc86
Denzale Houston <denzalethouston@gmail.com>
2020-10-09 12:38:29 -0700

    added ajax

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,16 @@
-$(document).ready( function(){
+$(document).ready(function () {
+   let city = $("#city").val();
+   let key = 'appid=0ccd28292b38949c924be67f905d975a';
+    $('#submitWeather').click(funtion(){
 
-let apiKey = "d7daca23bb60b5cdedb491704005803bS";
-const state = $("input").val();
-
-///first call
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=malibu&appid=d7daca23bb60b5cdedb491704005803bS",
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
-});
-
-///second call
-function currentWeather() {
+         if(city = ''){
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + 
+            "appid=0ccd28292b38949c924be67f905d975a",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+        });
+    }
     
-
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d7daca23bb60b5cdedb491704005803bS",
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
 });
-
-}
-
-
-///third call
-function fiveDayCast() {
-    
-$.ajax({
-    url: "pro.openweathermap.org/data/2.5/forecast/hourly?id=input&appid=d7daca23bb60b5cdedb491704005803bS",
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
-});
-
-}})
