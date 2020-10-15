78e0d3d79abca868875b317c7e967174940701cd
Denzale Houston <denzalethouston@gmail.com>
2020-10-15 15:46:19 -0700

    weather update

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
   let key = "920a4b29fbcfc27b5c48d10de6154ef9";
-  
+
+
   $("#submitWeather").on("click", weatherSearch)
 
   function weatherSearch() {
     const city = $('#city').val();
-    
-    console.log(city)
     const url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=imperial"
     console.log(url)
     $.ajax({
@@ -14,8 +13,17 @@ $(document).ready(function () {
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=imperial"
     }).then(function (data) {
       console.log(data)
-      let wind = $("<p>").addClass("card-text").text("Wind: ")
-    }
-    )
+      $("#city").html("<h3>" + data.name + "</h3>");
+      $("#wind").text("Wind Speed: " + data.wind.speed + " MPH");
+      $("#temp").text("Temperature: " + data.main.temp + " ℉");
+      $("#humidity").text("Humidity: " + data.main.humidity + "%");
+      console.log("Wind Speed: " + data.wind.speed);
+      console.log("Humidity: " + data.main.humidity);
+      console.log("Temperature: " + temp);
+    })
   };
+  
+  // Five-Day Forecast
+  
+
 });
