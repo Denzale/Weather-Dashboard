12c49fe0fc9531609d70e5e8b02652577de06eed
Denzale Houston <denzalethouston@gmail.com>
2020-10-21 15:35:04 -0700

    https update

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
 
   function weatherSearch() {
     const city = $('#city').val();
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=imperial"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=imperial"
     console.log(url)
     $.ajax({
       method: "GET",
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=imperial"
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=imperial"
     }).then(function (data) {
       console.log(data)
       $("#city").html("<h3>" + data.name + "</h3>");
@@ -22,8 +22,27 @@ $(document).ready(function () {
       console.log("Temperature: " + temp);
     })
   };
-  
+
   // Five-Day Forecast
   
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + key + "&units=imperial",
+  }).then(function (data) {
+    console.log(data)
+    $("#forecast").empty();
+    $("#forecast").append("<h1>5-Day Forecast:</h1>");
+
+    let makeDiv = $("<div>");
+    let upDate = $("<h1>");
+    let humidity = $("<p>");
+
+
+    makeDiv.addClass("row");
+    upDate = moment().add((i + 1), 'day').format('MM/DD/YYYY');
+    makeDiv.append(upDate)
+    humidity.text("Humidty: " + data.list)
+  
+  })
 
 });
